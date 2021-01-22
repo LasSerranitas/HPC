764c7b959b23d02634fd67e72e6bf1beedd134fe
LasSerranitas <56177563+LasSerranitas@users.noreply.github.com>
2021-01-22 02:10:59 -0300

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,29 +1,31 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Numpy array import
 import numpy as np
+
+# Import python parallel processing
 import multiprocessing as mp
 
-
-def fib(num):
+# Tethod that calculates fibonacci with recursion 
+def fib(num):    
     if num < 2:
         return str(num)
     else:
-        # fn = fn-1 + fn-2
+        # Returns the sum of the two previous numbers
         return str(int(fib(num - 2)) + int(fib(num - 1)))
 
-
-def factorize(number,d):
+# Factorize the number
+def factorize(number,d):    
     print('{} : {} = '.format(d, number), end='')
     if number == '0':
         print(' {} '.format(number), end='')
     if number == '1':
         print(' {} '.format(number), end='')
+    # Result array
     resultado = np.arange(100)
     for j in range(100):
         resultado[j] = '0'
+    # Instance number
     i = '2'
+    # Posision number
     pos = 0
     while True:
         if str(int(number) % int(i)) == '0':
@@ -39,9 +41,10 @@ def factorize(number,d):
             if number != '1':
                 resultado[pos] = number
             break
-
+    # Aux number
     exam = resultado[0]
     cantidad = 0
+    # Transform resutlado a string and print the result
     for i in range(100):
         if exam == resultado[i]:
             cantidad = cantidad + 1
@@ -50,7 +53,6 @@ def factorize(number,d):
                 print(' {} '.format(exam), end='')
             else:
                 print(' {} ^ {}'.format(exam, cantidad), end='')
-
             exam = resultado[i]
             cantidad = 1
             if resultado[i] == 0:
@@ -61,8 +63,10 @@ def factorize(number,d):
 
 if __name__ == '__main__':
     #freeze_support()
+    # Create a pool whit the max core
     pool = mp.Pool(mp.cpu_count())
+    # Create all instance fib
     for i in range(300):
+        # Calculate a fibonacci number and factorize whit parallelization
         pool.apply_async(factorize(fib(i),i))
-    pool.close()
-    pool.join()
+    
